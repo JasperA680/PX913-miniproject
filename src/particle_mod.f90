6c24d05d3007f3ba13700ecef9c784ccc09d834a
Anton-Oleinik <anton.oleinik@warwick.ac.uk>
2025-12-02 15:29:05 +0000

    Started particle class, wrote initialiser subroutine

--- src/particle_mod.f90
+++ src/particle_mod.f90
@@ -15,17 +15,76 @@
 !> --------------
 
 MODULE particle
+  USE ISO_FORTRAN_ENV
   IMPLICIT NONE
+  SAVE
+
+  INTEGER, PARAMETER :: numIters = 1000
+  INTEGER, PARAMETER :: q = -1
+  REAL(REAL64), PARAMETER :: dt = 0.01_REAL64
 
   TYPE particle
+    !> Current position, velocity, acceleration of the particle
+    REAL(REAL64), DIMENSION(2) :: pos, vel, acc
 
+    !> Histories of the x- and y- positions, velocities, accelerations
+    REAL(REAL64), DIMENSION(:), ALLOCATABLE :: histPosX, histPosY, &
+                           histVelX, histVelY, histAccX, histAccY
 
-  END TYPE
+    !> Electric fields to calculate acceleration
+    REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: electricX, electricY
+
+    CONTAINS
+
+    PROCEDURE :: init => particleInit
+
+  END TYPE particle
 
   CONTAINS
 
-  SUBROUTINE 
+  !> Initialises particle position and velocity, allocates history arrays,
+  !> and copies electric field arrays
+  SUBROUTINE particleInit(this, mode, eX, eY)
+    CLASS(particle), INTENT(INOUT) :: this
+    CHARACTER(LEN=*), INTENT(IN) :: mode
+    REAL(REAL64), DIMENSION(0:, 0:), INTENT(IN) :: eX, eY ! Arrays for electric field components
+
+    ALLOCATE(this%histPosX(0:numIters))
+    ALLOCATE(this%histPosY(0:numIters))
+    ALLOCATE(this%histVelX(0:numIters))
+    ALLOCATE(this%histVelY(0:numIters))
+    ALLOCATE(this%histAccX(0:numIters))
+    ALLOCATE(this%histAccY(0:numIters))
+
+    !> Sets the initial conditions
+    SELECT CASE (TRIM(mode))
+      CASE ("null")
+        this%pos = [0.0_REAL64, 0.0_REAL64]
+        this%vel = [0.1_REAL64, 0.1_REAL64]
+      CASE ("single")
+        this%pos = [0.1_REAL64, 0.0_REAL64]
+        this%vel = [0.0_REAL64, 0.0_REAL64]
+      CASE ("double")
+        this%pos = [0.0_REAL64, 0.5_REAL64]
+        this%vel = [0.0_REAL64, 0.0_REAL64]
+    END SELECT
+
+    this%histPosX(0) = this%pos(0)
+    this%histPosY(0) = this%pos(1)
+    this%histVelX(0) = this%vel(0)
+    this%histVelY(0) = this%vel(1)
+
+    this%electricX = eX
+    this%electricY = eY
+
+  END SUBROUTINE particleInit
 
 
 
 END MODULE particle
+
+
+
+
+
+
